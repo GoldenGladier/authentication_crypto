2318a64bf7ad875990b326a89673a6eab700e433
Omar Aguirre <omar.fi.wwr@gmail.com>
2021-12-04 01:12:29 -0600

    Se cambio configuracion de host permitidos

--- authentication_crypto/settings.py
+++ authentication_crypto/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v%cw&xl6%!!5k$6o0448@=##1t&9^=s&&z8b3v22#dq+04+j5j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://omaraguirre.pythonanywhere.com/', 'omaraguirre.pythonanywhere.com']
 
 
 # Application definition
