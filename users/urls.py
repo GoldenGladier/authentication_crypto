f3f5f4654deaa2e61602f5c0eb4c12e6f36d6dec
Omar Aguirre <omar.fi.wwr@gmail.com>
2021-12-04 19:02:31 -0600

    Últimos cambios funcionando

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('logout/', logout_view, name="logout-view"),
     path('validateuser/', username_validate_view, name="username-validate-view"),
     path('home/', home_view, name="home-view"),
+    # path('/', home_view, name="home-view"),
     # path('accounts/', include('django.contrib.auth.urls')),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'), name='password_reset_done'),
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password/password_reset_confirm.html"), name='password_reset_confirm'),
